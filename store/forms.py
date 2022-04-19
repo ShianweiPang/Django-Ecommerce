725ea7643af979a75781127f437ef47fa6f177ef
Shianwei <pshianwei@gmail.com>
2022-04-19 18:38:04 +0800

    Create User Form

--- store/forms.py
+++ store/forms.py
@@ -11,3 +11,9 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            # visible.field.widget.attrs['id']="form3Example4cd"
